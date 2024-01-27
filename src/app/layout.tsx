9ad8b9815004043cdbbc6f0405dc1cdf0a310808
Max Postnikov <max@postnikov.dev>
2024-01-27 13:27:17 -0800

    icons paths changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang="en">
 			<head>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
 				<link rel="manifest" crossOrigin="use-credentials" href="./manifest.json" />
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0e7040" />
 				<meta name="msapplication-TileColor" content="#0e7040" />
