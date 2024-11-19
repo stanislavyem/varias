f28240d7110e70b7770e18131f28cc201a57a5ea
Stanislav Yem <002yem@gmail.com>
2024-11-18 23:06:31 -0800

    update name and add underline to links

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
 	return (
-		<section className="about py-10" role="contentinfo" aria-label="Information about DateMarks.">
+		<section className="about py-10" role="contentinfo" aria-label="Information about Datemarks.">
 			<h2 className='mt'>About us</h2>
 		</section>
 	)
